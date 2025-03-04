5526ad8c03be3d7c2b32f5eea99ac42ef7dd3d5b
bogom1l <bogomil.stoev01@gmail.com>
2025-03-04 20:55:34 +0200

    imported global.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import "./src/styles/global.css";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
