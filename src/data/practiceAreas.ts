8a5854953b5d1ab0102c3243d984861b1dfa44d7
bogom1l <bogomil.stoev01@gmail.com>
2025-03-04 14:08:07 +0200

    Added PracticeAreas Page1 ( Family Law )

--- src/data/practiceAreas.ts
+++ src/data/practiceAreas.ts
@@ -2,36 +2,42 @@ export const practiceAreas = [
     {
       id: 1,
       title: "Семейно право",
+      url: "family-law",
       description: "Управление на разводи, въпроси по попечителство и семейни спорове с внимание и опит.",
       image: "src/assets/family-law.jpg"
     },
     {
       id: 2,
       title: "Криминална защита",
+      url: "criminal-defense",
       description: "Предоставяне на здрави защитни стратегии за различни криминални обвинения.",
       image: "src/assets/criminal-defense.jpg"
     },
     {
       id: 3,
       title: "Имотно право",
+      url: "real-estate-law",
       description: "Експертно съдействие при имотни сделки, спорове и регулации.",
       image: "src/assets/real-estate-law.jpg"
     },
     {
       id: 4,
       title: "Трудово право",
+      url: "employment-law",
       description: "Консултиране относно правата на работното място, договори и спорове както за служители, така и за работодатели.",
       image: "src/assets/employment-law.jpg"
     },
     {
       id: 5,
       title: "Наследствено планиране",
+      url: "estate-planning",
       description: "Помагане на клиентите да осигурят своето бъдеще чрез завещания, доверителни фондове и цялостно управление на имуществото.",
       image: "src/assets/estate-planning.jpg"
     },
     {
       id: 6,
       title: "Интелектуална собственост",
+      url: "intellectual-propety",
       description: "Защита на креативни идеи, търговски марки и иновации чрез експертни юридически съвети.",
       image: "src/assets/intellectual-property.jpg"
     },
